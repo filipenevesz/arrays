007445c66f56123d1e47c09f807c8e10fae56e33
Filipe <fedzn11@gmail.com>
2022-01-14 14:48:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//               0       1        2          3       4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+
+alunos.push('Luiza')
+alunos.push('Eduardo')
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array);
+
+/* const removido = alunos.shift();
+console.log(removido);
+console.log(alunos); */
+
+//
+//
+//
+
+//alunos.unshift('Luiza')
+// alunos.unshift('Fabio')
+
+
+//alunos[alunos.length] = 'Luiza'
+//alunos[alunos.length] = 'Fábio'
+//alunos[alunos.length] = 'Luana'
+
+
+
+//alunos[0] = 'Eduardo'
+//alunos[3] = 'Luiza'
+//console.log(alunos[0]);
+//console.log(alunos[2]);
